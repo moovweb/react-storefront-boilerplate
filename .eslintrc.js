6a943b640e95c392e1e7c1c94ac198ae63d510f1
Richard van der Dys <richard.vanderdys@gmail.com>
2018-12-04 19:11:12 -0600

    Using JS config of eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 // Use the default moovweb eslint style guide by running
 // `npm install eslint-config-moov`, or use your own style guide.
 //
-// module.exports = {
-//   extends: 'moov'
-// };
+module.exports = {
+	extends: 'moov'
+};
