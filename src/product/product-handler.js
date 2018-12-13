8816fa86e856aec66ffa0ad84e37f5a56d82fe65
Richard van der Dys <richard.vanderdys@gmail.com>
2018-12-13 13:55:02 -0800

    Fixing breadcrumbs

--- src/product/product-handler.js
+++ src/product/product-handler.js
@@ -4,16 +4,21 @@ import { withGlobalState } from 'react-storefront/router'
 import { fetchProduct } from 'react-storefront-extensions/shopify'
 
 export default function productHandler({ id, c, s }, request, response) {
+  const breadcrumbs = [
+    { text: 'Home', url: '/' }
+  ];
+  if (c) {
+    breadcrumbs.push({ text: `Shopify Collections`, url: `/c/${c}` });
+  }
+  if (s) {
+    breadcrumbs.push({ text: `Subcategory ${s}`, url: `/s/${s}?c=${c}` });
+  }
   return fetchProduct(id).then(product => {
+    breadcrumbs.push({ text: product.name });
     return withGlobalState(request, globalState, { 
       title: product.name,
       page: 'Product',
-      breadcrumbs: [
-        { text: 'Home', url: '/' },
-        { text: `Shopify Collections`, url: `/c/0` },
-        { text: `Subcategory ${s}`, url: `/s/${s}?c=${c}` },
-        { text: product.name },
-      ],
+      breadcrumbs,
       product
     });
   }); 
