ed5bb73303d36397b24ce80ff166d15cb66454a9
kevhender <kevhender@gmail.com>
2018-12-13 16:43:07 -0800

    Add variant image handling

--- src/product/images-handler.js
+++ src/product/images-handler.js
@@ -1,13 +1,5 @@
-export default function imagesHandler({ id, color }) {
-  const images = [
-    `https://via.placeholder.com/600x600/${color}/fff?text=${encodeURIComponent('Product Image 1')}`,
-    `https://via.placeholder.com/600x350/${color}/fff?text=${encodeURIComponent('Product Image 2')}`,
-    `https://via.placeholder.com/350x600/${color}/fff?text=${encodeURIComponent('Product Image 3')}`,
-  ]
+import { fetchProductImages } from 'react-storefront-extensions/shopify'
 
-  return {
-    images,
-    thumbnails: images, 
-    thumbnail: images[0]
-  }
+export default async function imagesHandler({ id, color }) {
+  return fetchProductImages(id, color);
 }
