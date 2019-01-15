b6031f5ee44a7ba9989bfcf6bd37c90674dcc1e4
Richard van der Dys <richard.vanderdys@gmail.com>
2019-01-15 23:18:18 +0200

    Adding express option
    https://moovweb.atlassian.net/browse/PRPL-1859

---
+++ src/expressServer.js
@@ -0,0 +1,44 @@
+import express from 'express'
+import cors from 'cors'
+import bodyParser from 'body-parser'
+import theme from './theme'
+import model from './AppModel'
+import App from './App'
+import router from './routes'
+import Server from 'react-storefront/Server'
+// import reactStorefrontMiddleware from 'react-storefront-middleware'
+import reactStorefrontMiddleware from './middleware'
+import paths from '../config/paths'
+import chalk from 'chalk'
+import path from 'path'
+
+const app = express()
+
+// Use Nginx or Apache to serve static assets in production or remove the if() around the following
+// lines to use the express.static middleware to serve assets for production (not recommended!)
+if (process.env.NODE_ENV === 'development') {
+  app.use(paths.publicPath, express.static(path.join(paths.clientBuild, paths.publicPath)))
+}
+
+app.use(cors())
+app.use(bodyParser.json())
+
+app.use(
+  reactStorefrontMiddleware(
+    new Server({ 
+      theme, 
+      model, 
+      App, 
+      router
+    }) 
+  )
+)
+
+app.listen(process.env.PORT || 8500, () => {
+  console.log(
+    `[${new Date().toISOString()}]`,
+    chalk.blue(`App is running: 🌎 http://localhost:${process.env.PORT || 8500}`)
+  )
+})
+
+export default app
