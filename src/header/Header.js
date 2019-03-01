0a758afa9cc2fee9e97944b16b0beff728063759
Mark Brocato <mark.brocato@moovweb.com>
2019-03-01 02:32:58 -0800

    hide text on menu button by default

--- src/header/Header.js
+++ src/header/Header.js
@@ -70,7 +70,7 @@ export default class Header extends Component {
 
     return (
       <div>
-        <AppBar classes={{ root: classes.root }} menuAlign="right">
+        <AppBar classes={{ root: classes.root }} menuAlign="right" menuIconProps={{ label: false }}>
           <Menu align="right" useExpanders/>
           <Hidden mdUp implementation="css">{ storeFinder }</Hidden>
           <Hidden mdUp implementation="css">
