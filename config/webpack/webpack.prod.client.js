bc6891da9eb560a1c4ef0a1e95eaaf41867691b3
Mark Brocato <mark.brocato@moovweb.com>
2019-05-13 03:32:29 -0700

    update RSF

--- config/webpack/webpack.prod.client.js
+++ config/webpack/webpack.prod.client.js
@@ -3,6 +3,8 @@ const path = require('path')
 
 module.exports = prod(path.join(__dirname, '..', '..'), {
   workboxConfig: require('./workbox.config'),
+  prefetchRampUpTime: 0,
+  allowPrefetchThrottling: true,
   entries: {
     header: './proxy/hydrateHeader'
   }
