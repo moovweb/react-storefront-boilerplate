4bb883c1d10127c98e2d55bee19fca1a4ca7d202
Mark Brocato <mark.brocato@moovweb.com>
2019-05-20 03:26:03 -0700

    Add rewriteCookies

--- scripts/moov_response_header_transform.js
+++ scripts/moov_response_header_transform.js
@@ -1,5 +1,7 @@
 const responseHeaderTransform = require('react-storefront-moov-xdn/responseHeaderTransform').default
+const { rewriteCookies } = require('react-storefront-extensions/cookies')
 
 module.exports = function() {
   responseHeaderTransform()
+  rewriteCookies()
 }
