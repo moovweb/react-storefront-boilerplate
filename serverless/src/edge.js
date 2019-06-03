f119ad34c4d841283b657ed8384077fc61ee86f2
Richard van der Dys <richard.vanderdys@gmail.com>
2019-06-03 15:41:56 +0300

    Using helper function

--- serverless/src/edge.js
+++ serverless/src/edge.js
@@ -1,21 +1,13 @@
+import addCacheKey from 'react-storefront/edge'
 import router from "../../src/routes";
-import { CLOUDFRONT_CACHE, XDN_VERSION } from 'react-storefront/router/headers'
 
-function buildCacheKey(match, params) {
-  if (!match) return null
-  return params.id
-}
+export const handler = (...args) => {
+  addCacheKey(router, ...args)((match, params) => {
 
-export const handler = (event, context, callback) => {
+    console.log('match', match)
+    console.log('params', params)
 
-  const request = event.Records ? event.Records[0].cf.request : event;
-    
-  const { match, params } = router.findMatchingRoute(request)
-  console.log('match', match);
-  console.log('params', params);
-
-  request.headers[CLOUDFRONT_CACHE] = buildCacheKey(match, params)
-  request.headers[XDN_VERSION] = __build_timestamp__ // eslint-disable-line
-
-  callback(null, request)
+    if (!match) return null
+    return params.id
+  })
 };
