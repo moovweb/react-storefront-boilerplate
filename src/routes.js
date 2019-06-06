76cd78c878b8e889c281125ba690eac297066a7f
Matthew Tuusberg <tuusberg@gmail.com>
2019-06-06 11:43:10 +0300

    make surrogateKey a fucntion

--- src/routes.js
+++ src/routes.js
@@ -65,7 +65,7 @@ export default new Router()
     cache({
       server: {
         maxAgeSeconds: 300,
-        surrogateKey: 'skproduct'
+        surrogateKey: () => 'skproduct'
       },
       client: true
     }),
