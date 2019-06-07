9097d3197ad91deb59308748d7edafab4c87bb6c
Matthew Tuusberg <tuusberg@gmail.com>
2019-06-07 21:54:12 +0300

    properly get requests object from api gateway/L@E event

--- serverless/src/edge.js
+++ serverless/src/edge.js
@@ -3,27 +3,36 @@ import querystring from 'querystring'
 import { CLOUDFRONT_CACHE_HASH, XDN_VERSION } from 'react-storefront/router/headers'
 import router from "../../src/routes";
 
-const SURROGATE_KEY_NAME ='__moov_sk__'
+export const SURROGATE_KEY_NAME ='__moov_sk__'
+
+
+const getRequest = (event, isAtEdge) => {
+  let request = isAtEdge ? event.Records[0].cf.request : event
+  if (isAtEdge) {
+    request.path = request.uri
+    request.query = querystring.parse(request.querystring)
+  }
+
+  return request
+}
 
 export const handler = (event, context, callback) => {
-
   const isAtEdge = !!event.Records
-  const version = process.env.MOOV_XDN_VERSION || __build_timestamp__ // eslint-disable-line
-  const request = isAtEdge ? event.Records[0].cf.request : event
-  
-  const query = request.querystring ? querystring.parse(request.querystring) : request.query
+  const request = getRequest(event, isAtEdge)
+  const query = request.query
 
   // TODO: Clean up what is passed to Router
   const cacheKey = router.getCacheKey({
     path: request.uri,
     headers: request.headers,
-    query
+    query: request.query
   }, {
     path: request.uri || request.path,
     query: querystring.stringify(query),
     // TODO: Need production properties here
   })
 
+  const version = process.env.MOOV_XDN_VERSION || __build_timestamp__ // eslint-disable-line
   // Inject after user transforms it
   cacheKey.version = version
   
@@ -50,13 +59,13 @@ export const handler = (event, context, callback) => {
   setHeader(request, CLOUDFRONT_CACHE_HASH, keyHash)
   setHeader(request, XDN_VERSION, version)
   setHeader(request, 'x-moov-edge-event', JSON.stringify(event))
-
+  
   const surrogateKey = router.getSurrogateKey(request)
   console.log('surrogateKey', surrogateKey);
   
   if (surrogateKey) {
     const keys = Array.isArray(surrogateKey) ? surrogateKey : [surrogateKey]
-    request.uri = request.uri + '/' + keys.map(k => `${SURROGATE_KEY_NAME}${k}`).join('/')
+    request.uri = `/${keys.map(k => `${SURROGATE_KEY_NAME}${k}`).join('/')}${request.uri}`
     console.log('request.uri', request.uri);
   }
 
