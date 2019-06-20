c162cf20de7b47ba803507824ddbb33b4347d1dd
Mark Brocato <mark.brocato@moovweb.com>
2019-06-20 02:13:25 -0700

    Adds initialGroups to search results.

--- src/globalState.js
+++ src/globalState.js
@@ -54,6 +54,55 @@ export default () => {
     menu: {
       levels: [menu]
     },
-    tabs: menu
+    tabs: menu,
+    search: {
+      initialGroups: [
+        {
+          caption: 'Suggested Searches',
+          results: ['Red Dress', 'Blue Shoes', 'White Top'].map(text => ({
+            text,
+            url: `/search?q=${encodeURIComponent(text)}`
+          }))
+        },
+        {
+          caption: 'Suggested Categories',
+          results: [
+            { text: 'Category 1', url: '/c/1' },
+            { text: 'Category 2', url: '/c/2' },
+            { text: 'Category 3', url: '/c/3' }
+          ]
+        },
+        {
+          caption: 'Suggested Products',
+          results: [
+            {
+              text: 'Product 1',
+              url: '/p/1?s=1&c=1',
+              thumbnail: 'https://via.placeholder.com/120x120'
+            },
+            {
+              text: 'Product 2',
+              url: '/p/2?s=1&c=1',
+              thumbnail: 'https://via.placeholder.com/120x120'
+            },
+            {
+              text: 'Product 3',
+              url: '/p/3?s=1&c=1',
+              thumbnail: 'https://via.placeholder.com/120x120'
+            },
+            {
+              text: 'Product 4',
+              url: '/p/4?s=1&c=1',
+              thumbnail: 'https://via.placeholder.com/120x120'
+            },
+            {
+              text: 'Product 5',
+              url: '/p/5?s=1&c=1',
+              thumbnail: 'https://via.placeholder.com/120x120'
+            }
+          ]
+        }
+      ]
+    }
   }
 }
