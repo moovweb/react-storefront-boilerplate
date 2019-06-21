b82e6c64a22877ccacffcf41e6999bff3396443e
Mark Brocato <mark.brocato@moovweb.com>
2019-06-20 21:30:32 -0700

    turn on prefetch throttling

--- config/webpack/webpack.prod.client.js
+++ config/webpack/webpack.prod.client.js
@@ -3,6 +3,7 @@ const path = require('path')
 
 module.exports = prod(path.join(__dirname, '..', '..'), {
   workboxConfig: require('./workbox.config'),
+  allowPrefetchThrottling: true,
   entries: {
     header: './proxy/hydrateHeader'
   }
