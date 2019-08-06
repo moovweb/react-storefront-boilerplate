2a87a2ea91d56a5f03a8a4c416c284664fa5dd91
Adrien Kiren <adrien.kiren@gmail.com>
2019-08-06 14:00:29 +0300

    fix: restore moov_edge file to not break moov_builder

---
+++ scripts/moov_edge_request_transform.js
@@ -0,0 +1,8 @@
+/**
+ * This function runs at the edge before the cache.
+ * @param {Object} options
+ * @param {Function} options.setCacheKey A function to register a callback to set a cache key for the current request.
+ */
+module.exports = ({ setCacheKey }) => {
+
+};
---
+++ scripts/moov_edge_response_transform.js
@@ -0,0 +1,6 @@
+/**
+ * This function runs at edge after the response has been returned from the cache.
+ */
+module.exports = function() {
+  // Put your custom logic to transform the response post-cache here if needed.
+};
