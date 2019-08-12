929398862ac45c837ccebceecb14b7b944e0cf2c
Mark Brocato <mark.brocato@moovweb.com>
2019-08-12 09:58:03 -0700

    fixed oem build

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,17 @@
-import { Router, fromClient, fromServer, cache, proxyUpstream } from 'react-storefront/router'
+import {
+  Router,
+  fromClient,
+  fromServer,
+  cache,
+  proxyUpstream,
+  createCustomCacheKey
+} from 'react-storefront/router'
 
 // See guide to caching on Moov XDN: https://pwa.moovweb.com/guides/caching
-const cacheHandler = cache({ server: { maxAgeSeconds: 300 }, client: true }) // cache responses in varnish for 5 minutes
+const cacheHandler = cache({
+  server: { maxAgeSeconds: 300, key: createCustomCacheKey().addCookie('moov-location') },
+  client: true
+}) // cache responses in varnish for 5 minutes
 
 // See guide to routing: https://pwa.moovweb.com/guides/routing
 export default new Router()
