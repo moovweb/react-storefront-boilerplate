a47dea90f05c7295c9ea02a185f9ea7ce1db4596
Leonid Tolli <letoll@Leonid-Tolli.local>
2019-08-21 20:07:33 +0300

    add smoke tests

--- test/smoke/smokeTests.spec.js
+++ test/smoke/smokeTests.spec.js
@@ -1,5 +1,58 @@
+const {
+  createDefaultDriver,
+  clickElement,
+  findVisibleElements,
+  waitForElement
+} = require('react-storefront-selenium')
+
 describe('smoke tests', () => {
-  it('should fail', () => {
-    expect(false).toBe(true)
+  jest.setTimeout(30000)
+  let driver
+
+  beforeAll(() => {
+    // driver = new Builder()
+    //   .setChromeOptions(createDefaultChromeOptions())
+    //   .forBrowser('chrome')
+    //   .build()
+
+    driver = createDefaultDriver()
+  })
+
+  afterAll(async () => {
+    await driver.quit()
+  })
+
+  it('Navigate to landing page', async function() {
+    await driver.get(host)
+  })
+
+  it('Navigate to category', async function() {
+    await clickElement(driver, '[data-th="nav"]')
+  })
+
+  it('navigate to subcategory', async function() {
+    await clickElement(driver, '[data-th="subcategory-link"]')
+  })
+
+  it('navigate to product', async function() {
+    await clickElement(driver, '[data-th="product-link"]')
+  })
+
+  it('Add product to cart ', async function() {
+    await clickElement(driver, '[data-th="add-to-cart"]')
+  })
+
+  it('Navigate to cart', async function() {
+    await clickElement(driver, '[data-th="cart-link"]')
+  })
+
+  it('Verify product in cart', async function() {
+    await waitForElement(driver, '[data-th="product-link"]')
+    const products = await findVisibleElements(driver, '[data-th="product-link"]')
+    expect(products).toHaveLength(1)
+  })
+
+  it('navigate to checkout', async function() {
+    await clickElement(driver, '[data-th="checkout-link"]')
   })
 })
