405b50fbea6c85c8bd93acb24cc018ee93643920
Matthew Tuusberg <tuusberg@gmail.com>
2019-08-27 13:45:58 +0300

    Update smokeTests.spec.js

--- test/smoke/smokeTests.spec.js
+++ test/smoke/smokeTests.spec.js
@@ -1,24 +1,17 @@
 /**
- * This is the default smoke test run by Moovweb Control Center when your project is linked to a GitHub repository
- * with the Auto-Deploy option checked
+ * This is the default smoke test run by Moovweb Control Center when
+ * your project is linked to a GitHub repository with the Auto-Deploy option checked.
  *
  * We rely on elements with `data-th` attributes to guide the selenium test script through the
  * shopping flow.
  *
- * The hostname of the app to be tested is specified in the required environment variable `RSF_HOST`.
+ * The hostname of the app to be tested is specified in the required environment variable `RSF_URL`.
  */
 
-require('chromedriver')
-
-const {
-  createDefaultDriver,
-  clickElement,
-  findVisibleElements,
-  waitForElement
-} = require('react-storefront-selenium')
+const { createBrowser, createPage, clickElement } = require('react-storefront-puppeteer')
 
 const startURL = process.env.RSF_URL
-const sleepBetweenPages = process.env.RSF_SLEEP_BETWEEN_PAGES || 1000
+const sleepBetweenPages = process.env.RSF_SLEEP_BETWEEN_PAGES || 2000
 const headless = process.env.RSF_HEADLESS || 'true'
 
 if (!startURL) {
@@ -29,53 +22,55 @@ if (!startURL) {
   process.exit(1)
 }
 
-describe('Smoke Tests', () => {
+describe('smoke tests', () => {
   jest.setTimeout(30000)
-  let driver
 
-  beforeAll(() => {
-    driver = createDefaultDriver({ headless })
+  let browser
+  let page
+
+  beforeAll(async () => {
+    browser = await createBrowser({ headless })
+    page = await createPage(browser)
   })
 
   afterAll(async () => {
-    await driver.quit()
+    await browser.close()
   })
 
   beforeEach(async () => {
-    await driver.sleep(sleepBetweenPages)
+    await page.waitFor(sleepBetweenPages)
   })
 
-  it('Navigate to landing page', async function() {
-    await driver.get(startURL)
+  it('Navigate to landing page', async () => {
+    await page.goto(startURL)
   })
 
-  it('Navigate to category', async function() {
-    await clickElement(driver, '[data-th="nav"]')
+  it('Navigate to category', async () => {
+    await clickElement(page, '[data-th="nav"]')
   })
 
   it('Navigate to subcategory', async function() {
-    await clickElement(driver, '[data-th="subcategory-link"]')
+    await clickElement(page, '[data-th="subcategory-link"]')
   })
 
   it('Navigate to product', async function() {
-    await clickElement(driver, '[data-th="product-link"]')
+    await clickElement(page, '[data-th="product-link"]')
   })
 
   it('Add product to cart ', async function() {
-    await clickElement(driver, '[data-th="add-to-cart"]')
+    await clickElement(page, '[data-th="add-to-cart"]')
   })
 
   it('Navigate to cart', async function() {
-    await clickElement(driver, '[data-th="cart-link"]')
+    await clickElement(page, '[data-th="cart-link"]')
   })
 
   it('Verify product in cart', async function() {
-    await waitForElement(driver, '[data-th="product-link"]')
-    const products = await findVisibleElements(driver, '[data-th="product-link"]')
-    expect(products).toHaveLength(1)
+    const products = await page.$x('//a[contains(@href, "p/1")]')
+    expect(products.length).toBeTruthy()
   })
 
   it('Navigate to checkout', async function() {
-    await clickElement(driver, '[data-th="checkout-link"]')
+    await clickElement(page, '[data-th="checkout-link"]')
   })
 })
