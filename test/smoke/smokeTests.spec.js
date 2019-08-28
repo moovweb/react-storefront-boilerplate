79d07c2d90924ad9ce9458f7768e6cc07541cfee
Matthew Tuusberg <tuusberg@gmail.com>
2019-08-28 16:45:37 +0300

    Update smokeTests.spec.js

--- test/smoke/smokeTests.spec.js
+++ test/smoke/smokeTests.spec.js
@@ -11,8 +11,8 @@
 const { createBrowser, createPage, clickElement } = require('react-storefront-puppeteer')
 
 const startURL = process.env.RSF_URL
-const sleepBetweenPages = process.env.RSF_SLEEP_BETWEEN_PAGES || 2000
 const headless = process.env.RSF_HEADLESS || 'true'
+const ignoreHTTPSErrors = process.env.RSF_IGNORE_HTTPS_ERRORS === 'true'
 
 if (!startURL) {
   console.error(
@@ -29,16 +29,12 @@ describe('smoke tests', () => {
   let page
 
   beforeAll(async () => {
-    browser = await createBrowser({ headless })
+    browser = await createBrowser({ headless, ignoreHTTPSErrors })
     page = await createPage(browser)
   })
 
   afterAll(async () => {
     await browser.close()
-  })
-
-  beforeEach(async () => {
-    await page.waitFor(sleepBetweenPages)
   })
 
   it('Navigate to landing page', async () => {
@@ -50,15 +46,15 @@ describe('smoke tests', () => {
   })
 
   it('Navigate to subcategory', async function() {
-    await clickElement(page, '[data-th="subcategory-link"]')
+    await clickElement(page, '[data-page="Category"] [data-th="subcategory-link"]')
   })
 
   it('Navigate to product', async function() {
-    await clickElement(page, '[data-th="product-link"]')
+    await clickElement(page, '[data-page="Subcategory"] [data-th="product-link"]')
   })
 
   it('Add product to cart ', async function() {
-    await clickElement(page, '[data-th="add-to-cart"]')
+    await clickElement(page, '[data-page="Product"] [data-th="add-to-cart"]')
   })
 
   it('Navigate to cart', async function() {
@@ -66,7 +62,7 @@ describe('smoke tests', () => {
   })
 
   it('Verify product in cart', async function() {
-    await page.waitForSelector('[data-th=product-link]')
+    await page.waitForSelector('[data-page="Cart"] [data-th=product-link]')
   })
 
   it('Navigate to checkout', async function() {
