1367ee9e71c6e26ba439aef7c14f671557c6242d
Richard van der Dys <richard.vanderdys@gmail.com>
2019-09-19 11:15:27 +0300

    Added videos

--- src/product/images-handler.js
+++ src/product/images-handler.js
@@ -6,13 +6,32 @@ export default function imagesHandler({ id, color }) {
     `https://via.placeholder.com/600x350/${color}/fff?text=${encodeURIComponent(
       'Product Image 2'
     )}`,
-    `https://via.placeholder.com/350x600/${color}/fff?text=${encodeURIComponent('Product Image 3')}`
+    `https://via.placeholder.com/350x600/${color}/fff?text=${encodeURIComponent(
+      'Product Image 3'
+    )}`,
+    {
+      src: 'https://temp.media/video/?height=400&width=500&length=10',
+      video: true
+    }
+  ]
+
+  const thumbnails = [
+    `https://via.placeholder.com/600x600/${color}/fff?text=${encodeURIComponent(
+      'Product Image 1'
+    )}`,
+    `https://via.placeholder.com/600x350/${color}/fff?text=${encodeURIComponent(
+      'Product Image 2'
+    )}`,
+    `https://via.placeholder.com/350x600/${color}/fff?text=${encodeURIComponent(
+      'Product Image 3'
+    )}`,
+    'https://placehold.it/500x400'
   ]
 
   return {
     images,
-    thumbnails: images,
-    thumbnail: images[0],
+    thumbnails,
+    thumbnail: thumbnails[0],
     selectedImage: 0
   }
 }
