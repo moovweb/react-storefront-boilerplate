b7b2cd3d29d7b0026e6b713ad07f36b0b9b376c3
Mark Brocato <mark.brocato@moovweb.com>
2019-10-09 15:12:01 +0300

    more debug

--- src/debug/debug-handler.js
+++ src/debug/debug-handler.js
@@ -1,9 +1,5 @@
 import { getMode } from 'react-storefront-moov-xdn/manifest'
 
 export default function() {
-  return {
-    moovManifest: global.env.moovManifest,
-    moov_mode_name: global.env.moov_mode_name,
-    mode: getMode()
-  }
+  return global.env
 }
