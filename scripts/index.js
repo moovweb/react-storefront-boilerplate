f477832c724f7d1db8016077b2bc35fca6627636
Mark Brocato <mark.brocato@moovweb.com>
2019-11-09 11:58:16 +0200

    removed timing

--- scripts/index.js
+++ scripts/index.js
@@ -2,21 +2,19 @@ console.error = console.warn = console.log
 
 module.exports = function() {
   const profile = require('react-storefront/utils/profile').default
-  profile('timing-index', () => {
-    require('../src/analytics')
+  require('../src/analytics')
 
-    const index = require('react-storefront-moov-xdn').default
-    const { transformAmpHtml } = require('react-storefront-extensions/amp')
-    const errorReporter = require('../src/errorReporter').default
+  const index = require('react-storefront-moov-xdn').default
+  const { transformAmpHtml } = require('react-storefront-extensions/amp')
+  const errorReporter = require('../src/errorReporter').default
 
-    index({
-      theme: require('../src/theme').default,
-      model: require('../src/AppModel').default,
-      App: require('../src/App').default,
-      router: require('../src/routes').default,
-      blob: env.blob || require('../src/blob.dev'),
-      transform: transformAmpHtml,
-      errorReporter
-    })
+  index({
+    theme: require('../src/theme').default,
+    model: require('../src/AppModel').default,
+    App: require('../src/App').default,
+    router: require('../src/routes').default,
+    blob: env.blob || require('../src/blob.dev'),
+    transform: transformAmpHtml,
+    errorReporter
   })
 }
